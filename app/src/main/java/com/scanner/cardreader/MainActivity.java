21401557ccd02928c98c39184714ffc7182d40b8
Abhishek Bhatta <aviisekh@gmail.com>
2016-06-20 13:38:42 +0545

    Accessed the Camera

--- app/src/main/java/com/scanner/cardreader/MainActivity.java
+++ app/src/main/java/com/scanner/cardreader/MainActivity.java
@@ -1,7 +1,9 @@
 package com.scanner.cardreader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(getApplicationContext(),CameraActivity.class);
+        startActivity(intent);
     }
 }
