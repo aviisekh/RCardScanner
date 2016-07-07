ecf6c5485ccd1a53adaf7c1b938935fef51a8624
anush <developer.anushshrestha@gmail.com>
2016-07-07 11:25:02 +0545

    finalize image processsing

---
+++ app/src/main/java/com/scanner/cardreader/GrayScale.java
@@ -0,0 +1,10 @@
+package com.scanner.cardreader;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by anush on 7/5/2016.
+ */
+interface GrayScale {
+    Bitmap grayScale();
+}
---
+++ app/src/main/java/com/scanner/cardreader/Threshold.java
@@ -0,0 +1,11 @@
+package com.scanner.cardreader;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by anush on 7/4/2016.
+ */
+interface Threshold {
+    Bitmap threshold(Bitmap sourceImage);
+
+ }
