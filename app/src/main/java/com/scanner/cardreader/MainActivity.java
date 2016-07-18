9496c302b0e30763a855b1480ee7c4dfbe7c37e4
Pratik <pratik_star99@hotmail.com>
2016-07-18 17:06:34 +0545

    Sim Carrier

--- app/src/main/java/com/scanner/cardreader/MainActivity.java
+++ app/src/main/java/com/scanner/cardreader/MainActivity.java
@@ -9,16 +9,17 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button proceedBtn;
 
 
-    public static String SIM = "NTC"; //Global Variable to define the network carrier : NTC/NCELL
+    public static String SIM; //Global Variable to define the network carrier : NTC/NCELL
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        proceedBtn = (Button) findViewById(R.id.proceedBtn);
+
+        Button proceedBtn = (Button) findViewById(R.id.proceedBtn);
+        assert proceedBtn != null;
         proceedBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,17 +32,8 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
                 if (tm.getSimState() == TelephonyManager.SIM_STATE_READY) {
-                    String carrier = tm.getLine1Number().substring(0, 3);
-                    if (carrier.equals("984")) {
-                        SIM = "NTC";
-                    } else {
-                        SIM = "NCELL";
-                    }
-                } else {
-                    SIM = "Unknown";
+                    SIM = tm.getNetworkOperatorName();
                 }
-
-
             }
         });
         t.start();
